46fd4e3b7ec776a0e99f63394cefc3746d0b16e2
zhao <397957975@qq.com>
2020-11-07 18:07:33 +0800

    add test webview

--- app/src/main/java/com/zhaobiao/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/zhaobiao/myapplication/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,12 +17,15 @@ import com.zhaobiao.myapplication.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private WebView webView;
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        webView = (WebView) root.findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient()); //解决弹出系统浏览器进行网页访问选择问题
+        webView.loadUrl("http://ggfw.ywjypt.yw.gov.cn/index.html");
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -28,6 +33,7 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
         return root;
     }
 }
